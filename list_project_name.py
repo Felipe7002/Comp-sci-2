79c5f95fc7a4317d6cc2379a5fdf1fd02e1627ac
Felipe7002 <135037243+Felipe7002@users.noreply.github.com>
2024-02-21 09:19:03 -0500

    Update list_project_name.py

--- list_project_name.py
+++ list_project_name.py
@@ -19,3 +19,29 @@ def palindrome(name):
 check_vowels(name_input)
 print(f"Paildrone: {palindrome(name_input)}")
 
+def first_name(name):
+    first_name = ""
+    for letter in name:
+        if letter != " ":
+            first_name += letter
+        else:
+            break
+        return first_name
+def get_last_name(name):
+    last_name = ""
+    for i in range(len(name)-1, -1, -1):
+        if name[i] != " ":
+            last_name = name[i] + last_name
+        else:
+            break
+    return last_name
+def get_middle_names(name):
+    middle_names = ""
+    first_space = name.index(" ")
+    last_space = name.rindex(" ")
+    if first_space != last_space:
+        middle_names = name[first_space:last_space].strip()
+    return middle_names
+
+def
+
