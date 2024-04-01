e61cba22a7eadf0b5ca1693941be674b23ee32d4
Felipe7002 <135037243+Felipe7002@users.noreply.github.com>
2024-03-31 22:24:36 -0400

    Update shakespeare.py

--- shakespeare.py
+++ shakespeare.py
@@ -1,16 +1,15 @@
 
+
 play1_path = r"C:\Users\FMiguens26\Downloads\playwithboys1.txt"  
 play2_path = r"C:\Users\FMiguens26\Downloads\playwithboys2.txt"
 non_descriptive= ["a", "the", "of", "I", "or", "and", "to"]
-fname = input('Enter the file name: ')
-try:
-    fhand = open(fname)
-except:
-    print('File cannot be opened:', fname)
-    exit()
-
-counts = dict()
-for line in fhand:
+shakespeare_play = input('Which play do you want to graph?')
+if shakespeare_play == "1":
+    file = open(play1_path)
+if shakespeare_play == "2":
+    file = open(play2_path)
+counts = {}
+for line in file:
     words = line.split()
     for word in words:
         if word not in counts:
@@ -25,5 +24,6 @@ values = [30, 40, 20, 10]
 
 import plotly.graph_objects as go
 fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
-fig.update_layout(title='Fruit Distribution')
+fig.update_layout(title='Shakespeare word count')
 fig.show()
+
