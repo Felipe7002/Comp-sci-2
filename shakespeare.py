b02645d64e1ef410ab8a09a82f2b531a14823011
Felipe7002 <135037243+Felipe7002@users.noreply.github.com>
2024-04-02 09:29:33 -0400

    Update shakespeare.py

--- shakespeare.py
+++ shakespeare.py
@@ -1,8 +1,9 @@
 
 
+import plotly.graph_objects as go
 play1_path = r"C:\Users\FMiguens26\Downloads\playwithboys1.txt"  
 play2_path = r"C:\Users\FMiguens26\Downloads\playwithboys2.txt"
-non_descriptive= ["a", "the", "of", "I", "or", "and", "to"]
+non_descriptive = ["he", "to", "and", "for", "and", "nor", "thee", "thy", "but", "when", "from", "more", "enter", "am", "my", "in", "macb.", "his", "our", "haue", "he", "be", "the", "of", "i", "a", "that", "is", "you", "with", "not", "all", "thou", "they", "it", "your", "have", "this", "what", "as", "we", "which", "me", "so", "do", "on", "no", "[enter", "him", "at", "by", "will", "th", "if", "yet", "shall", "upon", "her", "are", "hath", "their", "first", "was", "would", "good", "should", "like", "us", "th'", "did", "make", "or", "must", "i'll", "'tis", "let", "who", "where", "now", "how", "had", "an", "come", "them", "great", "know", "say", "may", "than", "scene", "=======", "second", "[they", "exit.]", "see", "were", "o", "she", "o,", "then", "here"]
 shakespeare_play = input('Which play do you want to graph?')
 if shakespeare_play == "1":
     file = open(play1_path)
@@ -12,18 +13,25 @@ counts = {}
 for line in file:
     words = line.split()
     for word in words:
+        word = word.lower()
+        if word in non_descriptive: continue
         if word not in counts:
             counts[word] = 1
         else:
             counts[word] += 1
+final_play1 = sorted(counts.items(), key=lambda x:x[1], reverse = True)
+print(counts) 
 
-print(counts)
+final_play1 = final_play1[:10]
+final_play1 = dict(final_play1)
+top_values = final_play1.values()
+top_labels = final_play1.keys()
+
 
 labels = []
 values = [30, 40, 20, 10]
 
-import plotly.graph_objects as go
-fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+
+fig = go.Figure(data=[go.Pie(labels=list(top_labels), values=list(top_values))])
 fig.update_layout(title='Shakespeare word count')
 fig.show()
-
